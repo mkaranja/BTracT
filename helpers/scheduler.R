1f7b9eb6185f5ab9a5cb2fd02e9f647b1051b6f9
mkaranja <m.karanja@cgiar.org>
2019-05-23 11:43:47 +0300

    update

--- helpers/scheduler.R
+++ helpers/scheduler.R
@@ -10,10 +10,12 @@ df = df[Activity != "Flowering"][,-c(5:6)]
 df$`Days Elapsed` = Sys.Date()-df$Date
 
 # mean number of days between activities
-dff = bananadata
+dff = dplyr::left_join(bananadata, repeatpollination[,-1], by="Crossnumber")
 dff$`Days to Germination` = lubridate::date(dff$`Germination Date`) - lubridate::date(dff$`Embryo Rescue Date`)
 dff$`Days to Embryo Rescue` = lubridate::date(dff$`Embryo Rescue Date`) - lubridate::date(dff$`Seed Extraction Date`)
+dff$`Days to Repeat Pollination` = lubridate::date(dff$Repeat_Pollination_Date) - lubridate::date(dff$`First Pollination Date`)
 
+mean_days_to_repeatpollination = mean(as.integer(na.omit(dff$`Days to Repeat Pollination`))) %>% floor() # to repeat
 mean_days_to_maturity = mean(as.integer(na.omit(dff$`Days to Maturity`))) %>% floor() # to harvest
 mean_days_in_ripening = mean(as.integer(na.omit(dff$`Days in ripening shed`))) %>% floor() # to extraction
 mean_days_to_embryo_rescue = mean(as.integer(na.omit(dff$`Days to Embryo Rescue`))) %>% floor() # to rescue
@@ -23,11 +25,16 @@ mean_days_to_germination = mean(as.integer(na.omit(dff$`Days to Germination`))) 
 
 
 
-df$status = ifelse(df$Activity=='First pollination' & df$`Days Elapsed` > (mean_days_to_maturity+10), "Overdue",
-                ifelse(df$Activity=='First pollination' & df$`Days Elapsed` %between% c(mean_days_to_maturity-10, mean_days_to_maturity+10), "Ready",
-                    ifelse(df$Activity=='First pollination' & df$`Days Elapsed` %between% c(mean_days_to_maturity-30, mean_days_to_maturity-10), "Approaching",
-                           ifelse(df$Activity=='First pollination' & df$`Days Elapsed` < mean_days_to_maturity-30, "Wait",
-                  
+df$status = ifelse(df$Activity=='First pollination' & df$`Days Elapsed` > (mean_days_to_repeatpollination+5), "Overdue",
+                ifelse(df$Activity=='First pollination' & df$`Days Elapsed` %between% c(mean_days_to_repeatpollination-5, mean_days_to_repeatpollination+5), "Ready",
+                    ifelse(df$Activity=='First pollination' & df$`Days Elapsed` %between% c(mean_days_to_repeatpollination-10, mean_days_to_repeatpollination-5), "Approaching",
+                           ifelse(df$Activity=='First pollination' & df$`Days Elapsed` < mean_days_to_repeatpollination-10, "Wait",
+                
+              ifelse(df$Activity=='Repeat pollination' & df$`Days Elapsed` > (mean_days_to_maturity+10),"Overdue",
+                     ifelse(df$Activity=='Repeat pollination' & df$`Days Elapsed` %between% c(mean_days_to_maturity-10, mean_days_to_maturity+10), "Ready",
+                            ifelse(df$Activity=='Repeat pollination' & df$`Days Elapsed` %between% c(mean_days_to_maturity-30, mean_days_to_maturity-10), "Approaching",
+                                   ifelse(df$Activity=='Repeat pollination' & df$`Days Elapsed` < mean_days_to_maturity-30, "Wait",
+                                                              
                            # harvested
                   ifelse(df$Activity=='Harvested bunches' & df$`Days Elapsed` > mean_days_in_ripening+3,"Overdue",
                       ifelse(df$Activity=='Harvested bunches' & df$`Days Elapsed` %between% c(mean_days_in_ripening-3, mean_days_in_ripening+3), "Ready",
@@ -46,12 +53,13 @@ df$status = ifelse(df$Activity=='First pollination' & df$`Days Elapsed` > (mean_
                                                   ifelse(df$Activity=='Embryo Rescue'  & df$`Number of Embryo Rescued` >0 & df$`Days Elapsed` %between% c(45, 49), "Approaching",
                                                          ifelse(df$Activity=='Embryo Rescue'  & df$`Number of Embryo Rescued` >0 & df$`Days Elapsed` < 45, "Wait",NA
                                                        
-                                  ))))))))))))))))
+                                  ))))))))))))))))))))
 scheduler = df[!is.na(df$status),]
 scheduler = scheduler[status !='Wait']
-scheduler$`Next Activity` = ifelse(scheduler$Activity=="First pollination","Bunch Harvesting",
-                   ifelse(scheduler$Activity=="Harvested bunches","Seed extraction",
-                          ifelse(scheduler$Activity=="Seed extraction","Embryo Rescue",
-                          ifelse(scheduler$Activity=="Embryo Rescue","Germination",''))))
-scheduler = scheduler[,c("Location", "Accession","Date", "Activity","Days Elapsed", "status", "Next Activity")]
-
+scheduler$NextActivity = ifelse(scheduler$Activity=="First pollination","Repeat Pollination",
+                                   ifelse(scheduler$Activity=="Repeat pollination","Bunch Harvesting",
+                                          ifelse(scheduler$Activity=="Harvested bunches","Seed extraction",
+                                                 ifelse(scheduler$Activity=="Seed extraction","Embryo Rescue",
+                                                        ifelse(scheduler$Activity=="Embryo Rescue","Germination",'')))))
+scheduler = scheduler[,c("Location", "Accession","Date", "Activity","Days Elapsed", "status", "NextActivity")]
+colnames(scheduler)[4] = "CurrentActivity"
